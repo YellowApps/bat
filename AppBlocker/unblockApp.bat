8622f171ec09da685b23c7dee8d1c56871f6f470
YellowApps <96654406+YellowApps@users.noreply.github.com>
2022-09-01 22:56:25 +0300

    Add files via upload

---
+++ AppBlocker/unblockApp.bat
@@ -0,0 +1,23 @@
+::t.me/nekit270
+
+@echo off
+chcp 1251 > nul
+setlocal EnableExtensions
+
+if "%~1"=="" ( set /p "file=Введите имя EXE-файла программы: " ) else ( set "file=%~1" )
+for /f "tokens=*" %%i in ("%file%") do set "file=%%~nxi"
+
+reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\%file%" /f > nul 2> nul
+
+if "%errorlevel%"=="0" (
+	echo %file% успешно разблокирован.
+) else (
+	 echo Ошибка!
+	 net helpmsg %errorlevel% 2> nul
+	 echo Код: %errorlevel%
+)
+
+if "%~1"=="" pause > nul
+
+endlocal
+exit /b
