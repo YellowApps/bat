db8f2e661eae892568e4729468cc8a072ccd4738
YellowApps <96654406+YellowApps@users.noreply.github.com>
2022-09-19 16:28:10 +0300

    Add files via upload

--- getwifipass.bat
+++ getwifipass.bat
@@ -2,28 +2,29 @@
 chcp 1251 > nul
 setlocal EnableExtensions, EnableDelayedExpansion
 
-set format=csv
+set format=echo
 set outputFile=wifi_passwords
 set nextStr=0
 
 if "%~1"=="/?" (
-	echo %~n0 [/F ^<csv^|text^|echo^>] [/O ^<имя файла^>]
+	echo %~n0 [/F ^<csv^|text^|echo^|bat^>] [/O ^<имя файла^>]
 	echo Получает сохраненные в системе пароли от Wi-Fi-сетей.
 	echo.
-	echo /F ^<csv^|text^|echo^>    Формат данных:
+	echo /F ^<csv^|text^|echo^|bat^>    Формат данных:
 	echo                               CSV    Сохранить результат в csv-файл; 
 	echo                               TEXT   Сохранить результат в текстовый файл; 
 	echo                               ECHO   Вывести результат в консоль.
-	echo                       По умолчанию - csv.
+	echo                               BAT    Вывести результат в консоль в формате ИМЯ_СЕТИ:ПАРОЛЬ ^(для использования в пакетных файлах^).
+	echo                           По умолчанию - echo.
 	echo.
-	echo /O ^<имя файла^>   Имя выходного файла. Если формат данных - echo, этот параметр игнорируется.
+	echo /O ^<имя файла^>   Имя выходного файла. Если формат данных - echo или bat, этот параметр игнорируется.
 	exit /b
 )
 
 if "%~1"=="/f" (
-	if not "%~2"=="csv" if not "%~2"=="text" if not "%~2"=="echo" (
+	if not "%~2"=="csv" if not "%~2"=="text" if not "%~2"=="echo" if not "%~2"=="bat" (
 		echo Неправильный формат! 
-		echo Допустимые форматы: csv, text, echo.
+		echo Допустимые форматы: csv, text, echo, bat.
 		exit /b
 	)
 	set "format=%~2"
@@ -52,7 +53,8 @@ for /f "tokens=1,2* delims=:" %%i in ('netsh wlan show profile') do (
 			set "key=!key:~1!"
 			if "!format!"=="csv" echo !name!;!key! >> "!outputFile!"
 			if "!format!"=="text" echo Пароль от сети "!name!": !key! >> "!outputFile!"
-			if "!format!"=="echo" echo !name!:!key!
+			if "!format!"=="echo" echo Пароль от сети "!name!": !key!
+			if "!format!"=="bat" echo !name!:!key!
 		)
 	)
 	if !nextStr!==1 set nextStr=2
